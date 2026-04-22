2193457b514b9135b688aacd9820d194eaa55b1c
Eze Linda Somtochukwu <somtochukwueze95@gmail.com>
2026-04-22 01:31:36 +0100

    Update axiosConfig.js

--- .vscode/src/api/axiosConfig.js
+++ .vscode/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://34.239.170.112:8080',
+    baseURL: 'http://13.222.58.210:8080',
     headers: {
         'Content-Type': 'application/json',
     },
